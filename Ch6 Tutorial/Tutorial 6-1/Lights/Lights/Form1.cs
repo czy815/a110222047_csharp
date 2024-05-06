b08c95206c23baab20d93fc4b6bdae617572564c
czy815 <a110222047@mail.shu.edu.tw>
2024-05-06 17:57:50 +0800

    1130506

---
+++ Ch6 Tutorial/Tutorial 6-1/Lights/Lights/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lights
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void switchButton_Click(object sender, EventArgs e)
+        {
+            if(lightOnPictureBox.Visible==true)
+            {
+                TurnLightOff();
+            }
+            else
+            {
+                TurnLightOn();
+            }
+        }
+
+        public void TurnLightOff()
+        {
+            lightOffPictureBox.Visible = true;
+            lightOnPictureBox.Visible = false;
+            lightStateLabel.Text = "OFF";
+        }
+
+        public void TurnLightOn()
+        {
+            lightOffPictureBox.Visible = false;
+            lightOnPictureBox.Visible = true;
+            lightStateLabel.Text = "ON";
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lightOnPictureBox.Image = Properties.Resources.LightOn;
+            lightOffPictureBox.Image = Properties.Resources.LightOff;
+            lightOnPictureBox.Visible = false;
+            lightOffPictureBox.Visible = true;
+            lightStateLabel.Text = "OFF";
+        }
+    }
+}
